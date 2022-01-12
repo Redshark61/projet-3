b32ba19b3665ec22ba9fcb053606de288c325df3
Redshark61 <tim.vannier@gmail.com>
2022-01-12 11:20:45 +0100

    add way to choose the right departement

--- test.js
+++ test.js
@@ -12,3 +12,23 @@ input.addEventListener("keyup", function (e) {
         }
     });
 });
+
+const http = new XMLHttpRequest();
+const url = "https://raw.githubusercontent.com/Rudloff/french-postal-codes-api/master/insee.csv";
+http.open("GET", url);
+http.send();
+
+http.onreadystatechange = (e) => {
+    if (http.readyState === 4 && http.status === 200) {
+        const data = http.responseText;
+        const lines = data.split("\n");
+        const list = document.querySelector("datalist");
+        lines.forEach(function (line) {
+            const columns = line.split(";");
+            const option = document.createElement("option");
+            option.value = columns[0];
+            option.textContent = columns[1];
+            list.appendChild(option);
+        });
+    }
+};
