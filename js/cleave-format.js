5ab2504693cbf1da30f8f3f3d397fba4bd38c3d7
Redshark61 <tim.vannier@gmail.com>
2022-01-17 14:03:32 +0100

    added formatting to tel input + rename title

--- js/cleave-format.js
+++ js/cleave-format.js
@@ -1,9 +1,17 @@
 const inputsSecurityCode = document.querySelectorAll(".js-security-code");
+const telInputs = document.querySelectorAll(".js-tel-input");
 
 inputsSecurityCode.forEach((input) => {
     new Cleave(input, {
         delimiters: ["-", "-", "-", "-", "-", " "],
         blocks: [1, 2, 2, 2, 3, 3, 2],
         numericOnly: true,
+    });
+});
+
+telInputs.forEach((input) => {
+    new Cleave(input, {
+        phone: true,
+        phoneRegionCode: "FR",
     });
 });
